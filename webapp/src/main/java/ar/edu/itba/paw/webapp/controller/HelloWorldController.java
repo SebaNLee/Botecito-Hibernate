ce16790bade6dae00f209a86a230aa8f1a3920a6
SebaNLee <seba.nico.lee@gmail.com>
2026-03-16 13:02:17 -0300

    feat: implementation up to 02:22:00

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -3,15 +3,27 @@ package ar.edu.itba.paw.webapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import ar.edu.itba.paw.services.UserService;
 
 @Controller
 public class HelloWorldController {
+
+    private final UserService userService = new UserService();
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView helloWorld() {
         final ModelAndView mav = new ModelAndView("index.jsp");
         mav.addObject("message", "Hello World from Controller");
+        return mav;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+        public ModelAndView createUser(@RequestParam("email") final String email) {
+        final ModelAndView mav = new ModelAndView("index.jsp");
+        Object user = userService.createUser(email);
+        mav.addObject("message", "Hello World " + user.toString());
         return mav;
     }
 }

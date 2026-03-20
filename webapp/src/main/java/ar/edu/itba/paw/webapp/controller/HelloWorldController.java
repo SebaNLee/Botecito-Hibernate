0506b4154fb5193edd13216512d12b3d80bcd7a7
SebaNLee <seba.nico.lee@gmail.com>
2026-03-20 01:52:52 -0300

    build(pre-commit): add spotless plugin, palantir java format
    https://github.com/diffplug/spotless/tree/main/plugin-maven and https://github.com/palantir/palantir-java-format

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import ar.edu.itba.paw.services.UserService;
 
 @Controller
 public class HelloWorldController {
@@ -26,7 +26,7 @@ public class HelloWorldController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-        public ModelAndView createUser(@RequestParam("email") final String email) {
+    public ModelAndView createUser(@RequestParam("email") final String email) {
         final ModelAndView mav = new ModelAndView("index.jsp");
         Object user = userService.createUser(email);
         mav.addObject("message", "Hello World " + user.toString());

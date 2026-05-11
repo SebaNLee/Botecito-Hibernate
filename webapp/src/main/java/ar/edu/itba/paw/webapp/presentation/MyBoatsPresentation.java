8c4cf89aad87ba4b3b401815533cce1d52332fb9
SebaNLee <seba.nico.lee@gmail.com>
2026-05-11 13:46:15 -0300

    refactor(presentation): correct dir

--- webapp/src/main/java/ar/edu/itba/paw/webapp/presentation/MyBoatsPresentation.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/presentation/MyBoatsPresentation.java
@@ -1,4 +1,4 @@
-package ar.edu.itba.paw.webapp.presentation.nuevo;
+package ar.edu.itba.paw.webapp.presentation;
 
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.models.nuevo.MyBoatsItem;

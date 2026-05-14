d4d95e462d97dc83d1a99ad2a74e2980cc4830f5
SebaNLee <seba.nico.lee@gmail.com>
2026-05-14 14:27:41 -0300

    refactor(controller): review support inline

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/nuevo/ReviewController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/nuevo/ReviewController.java
@@ -1,7 +1,10 @@
 package ar.edu.itba.paw.webapp.controller.nuevo;
 
-import ar.edu.itba.paw.webapp.controller.support.nuevo.ReviewMvcSupport;
+import ar.edu.itba.paw.models.nuevo.UserModel;
+import ar.edu.itba.paw.services.nuevo.ReviewInterface;
+import ar.edu.itba.paw.webapp.controller.support.ToastSupport;
 import ar.edu.itba.paw.webapp.form.nuevo.ReviewForm;
+import ar.edu.itba.paw.webapp.presentation.AuthenticatedUserResolver;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -18,7 +21,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class ReviewController {
 
-    private final ReviewMvcSupport reviewMvcSupport;
+    private final ReviewInterface reviewInterface;
+    private final AuthenticatedUserResolver authenticatedUserResolver;
 
     @RequestMapping(value = "/reviews/booking/{bookingId:[0-9]+}", method = RequestMethod.POST)
     public ModelAndView createReview(
@@ -28,6 +32,34 @@ public class ReviewController {
             @RequestParam(value = "returnTo", required = false, defaultValue = "dashboard") final String returnTo,
             @RequestParam(value = "itemId", required = false) final Integer itemId,
             final RedirectAttributes redirectAttributes) {
-        return reviewMvcSupport.createReview(bookingId, form, errors, returnTo, itemId, redirectAttributes);
+        final UserModel currentUser = authenticatedUserResolver.currentAuthenticatedUser();
+        if (currentUser == null) {
+            return new ModelAndView("redirect:/login");
+        }
+
+        if (errors.hasErrors()) {
+            ToastSupport.error(redirectAttributes, "profile.reviews.validationError");
+            return reviewRedirect(returnTo, itemId);
+        }
+
+        final boolean created = reviewInterface
+                .createReviewForBooking(bookingId, currentUser.getId(), form.getRating(), form.getComment())
+                .isPresent();
+        if (created) {
+            ToastSupport.success(redirectAttributes, "profile.reviews.created");
+        } else {
+            ToastSupport.error(redirectAttributes, "profile.reviews.error");
+        }
+        return reviewRedirect(returnTo, itemId);
+    }
+
+    private static ModelAndView reviewRedirect(final String returnTo, final Integer itemId) {
+        if ("item".equals(returnTo) && itemId != null) {
+            return new ModelAndView("redirect:/item/" + itemId);
+        }
+        if ("dashboardHosting".equals(returnTo)) {
+            return new ModelAndView("redirect:/requests/incoming");
+        }
+        return new ModelAndView("redirect:/bookings#sent-booking-requests");
     }
 }

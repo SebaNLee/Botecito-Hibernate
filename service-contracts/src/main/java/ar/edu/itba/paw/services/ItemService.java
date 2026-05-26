e9837f9f789e98cab29d6d44e03f26d2a8764ed6
SebaNLee <seba.nico.lee@gmail.com>
2026-05-26 16:08:28 -0300

    feat(back): new listOwnerItems service, for /my-boats seachbar

--- service-contracts/src/main/java/ar/edu/itba/paw/services/ItemService.java
+++ service-contracts/src/main/java/ar/edu/itba/paw/services/ItemService.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 public interface ItemService {
 
-    ItemSearchResult listOwnerItems(int ownerId, int page, int pageSize);
+    ItemSearchResult listOwnerItems(int ownerId, String searchQuery, String status, String location,
+                                    int page, int pageSize, String sortBy);
 
     Item findItemById(int id);
 

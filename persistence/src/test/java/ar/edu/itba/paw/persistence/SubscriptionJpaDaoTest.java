d325ba3ccf2ea82e96ff911d08ba4ca554f4b759
SebaNLee <seba.nico.lee@gmail.com>
2026-06-03 20:54:02 -0300

    chore(test): holder files

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/SubscriptionJpaDaoTest.java
@@ -0,0 +1,16 @@
+package ar.edu.itba.paw.persistence;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestConfiguration.class)
+@Transactional
+public class SubscriptionJpaDaoTest {
+
+    @Autowired
+    private SubscriptionDao subscriptionDao;
+}
--- services/src/test/java/ar/edu/itba/paw/services/MailServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/MailServiceImplTest.java
@@ -1,3 +1,5 @@
 package ar.edu.itba.paw.services;
 
 // MailService are all void, will not test them, irrelevant
+
+// TODO tempaltes y mockear funcioamiento

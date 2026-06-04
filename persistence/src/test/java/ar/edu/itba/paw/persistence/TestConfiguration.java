21f79a66bf5bcaf677522b52db531e3cf63c953c
SebaNLee <seba.nico.lee@gmail.com>
2026-06-04 16:51:48 -0300

    feat(test)!: persistence TestConfiguration settings for Hibernate

--- persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfiguration.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfiguration.java
@@ -1,5 +1,8 @@
 package ar.edu.itba.paw.persistence;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import org.hsqldb.jdbc.JDBCDataSource;
@@ -17,12 +20,33 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 public class TestConfiguration {
 
+    private static final String[] PG_ENUM_TYPES = {
+            "item_status_enum",
+            "booking_status_enum",
+            "report_enum",
+            "target_enum",
+            "weekday_enum"
+    };
+
     @Bean
     public DataSource dataSource() {
         final JDBCDataSource dataSource = new JDBCDataSource();
         dataSource.setURL("jdbc:hsqldb:mem:paw_test;sql.syntax_pgs=true");
         dataSource.setUser("ha");
         dataSource.setPassword("");
+
+        // With JDBC we did a 1:1 representation of Flyway migration in HSQLDB relative syntax (ENUMs -> VARCHARs)
+        // We now don't use Flyway to recreate the HSQLDB schema for testing, and instead use the @Entitys themselves
+        // So now for Hibernate, this is the reasonable workaround (no really elegant tho)
+        try (Connection conn = dataSource.getConnection(); Statement stmt = conn.createStatement()) {
+            for (final String type : PG_ENUM_TYPES) {
+                stmt.execute("DROP DOMAIN IF EXISTS " + type);
+                stmt.execute("CREATE DOMAIN " + type + " AS VARCHAR(20)");
+            }
+        } catch (final SQLException e) {
+            throw new RuntimeException("HSQLDB persistence test setup failed", e);
+        }
+        
         return dataSource;
     }
 
@@ -34,7 +58,7 @@ public class TestConfiguration {
         factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
         factoryBean.setJpaPropertyMap(java.util.Map.of(
-                "hibernate.hbm2ddl.auto", "create-drop", // Using @Entity (no Flyway or HSQLDB needed in tests)
+                "hibernate.hbm2ddl.auto", "create-drop",
                 "hibernate.dialect", "org.hibernate.dialect.HSQLDialect",
                 "hibernate.show_sql", "true",
                 "hibernate.format_sql", "true"));
